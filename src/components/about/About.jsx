0cf628eea443fad081a7598f279e12092a41a03f
Chinepoh Divine-favour <divinechinepoh@gmail.com>
2023-06-05 05:20:30 +0100

    smalleredits

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <div className="about__me__image">
+          <div className="about__me-image">
             <img src={ME} alt="About__image" />
           </div>
         </div>
